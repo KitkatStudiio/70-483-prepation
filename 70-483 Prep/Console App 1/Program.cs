109e057570093c4b6f88eb8caabccb5fe99ccd31
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-11-16 12:42:12 +0530

    Initial Commit

---
+++ 70-483 Prep/Console App 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool stopped = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************************** THREADING ****************************");
+            Console.WriteLine("\n\n");
+            //ThreadProgram.AnotherThread();
+            //ThreadProgram.BackGroundThreads();
+            //ThreadProgram.ParameterizedThread();
+
+            // Stop Thread Program using Lambda Function
+            //Thread t = new Thread(new ThreadStart(() =>
+            //{
+            //    while (!stopped)
+            //    {
+            //        Console.WriteLine("Running...");
+            //        Thread.Sleep(1000);
+            //    }
+            //    Console.WriteLine("Thread is stopped");
+            //}));
+            //t.Start();
+            //Console.WriteLine("Press any key to stop the thread");
+            //Console.ReadKey();
+            //stopped = true;
+
+            //ThreadStaticDemo.ThreadStaticMethod();
+            //ThreadStaticDemo.ThreadMethod();
+            //Console.WriteLine("\n\n");
+            //Console.WriteLine("------------------- Some Extra Concepts of C# --------------------");
+            //Console.WriteLine("\n\n");
+            //Temprature.DisplayTemprature(DateTime.UtcNow, 12.5);
+
+        }
+    }
+    class Temprature
+    {
+        public static void DisplayTemprature(DateTime date, double temp)
+        {
+            string output;
+            //string lblMessage = output;
+            output = string.Format("Temprature at {0:t} on {0:dd/mm/yy}", date, temp);
+            Console.WriteLine(output);
+
+        }
+    }
+}
---
+++ 70-483 Prep/Console App 1/ThreadProgram.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public static class ThreadProgram
+    {
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Threadproc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+        public static void AnotherThread()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread: Do Some work. ");
+                Thread.Sleep(0);
+            }
+            t.Join();
+            Console.WriteLine("After Thread: Do Some work. ");
+        }
+
+        public static void ThreadMethod2()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProcess: {0}", i);
+                Thread.Sleep(1000);
+            } 
+        }
+        public static void BackGroundThreads()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod2));
+            t.IsBackground = true;// Changing isBackground to true will not reflect the changes application will quit
+            t.Start();
+
+            t.Join();
+        }
+
+        /// <summary>
+        /// This method takes an argument which will be passing when thread will be started
+        /// </summary>
+        /// <param object="o"></param>
+        public static void ThreadMethodWithParam(object o)
+        {
+            for(int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+            }
+        }
+        public static void ParameterizedThread()
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethodWithParam));
+            t.Start(15);
+            t.Join();
+        }
+    }
+}
